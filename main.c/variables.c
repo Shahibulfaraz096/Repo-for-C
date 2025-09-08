ee6a2c7d53ab59d913afb181081bae0d63166e20
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-09 01:40:55 +0600

    Add variables

---
+++ main.c/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 25;
+    int year = 2025;
+    int quality = 1;
+
+    printf("You are %d years old\n", age);
+    printf("The year is %d\n", year);
+    printf("You have ordered %d x items\n", quality);
+
+    return 0;
+}
