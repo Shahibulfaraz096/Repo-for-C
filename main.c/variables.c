1bca38d921cf7f442a5ae838eed48e2e25497e43
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-09 11:06:55 +0600

    Add variables

--- main.c/variables.c
+++ main.c/variables.c
@@ -10,5 +10,12 @@ int main()
     printf("The year is %d\n", year);
     printf("You have ordered %d x items\n", quality);
 
+    double pi = 3.1415934757324;
+
+    double e = 2.71127851751475;
+
+    printf("The value of pi is %.15lf\n", pi);
+    printf("The value of e is %.15lf\n", e);
+
     return 0;
 }
