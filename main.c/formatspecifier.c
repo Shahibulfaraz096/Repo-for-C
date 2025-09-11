a907ad6e01a07d11e700491f4af0be16d912aee3
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-11 15:41:36 +0600

    Add variables

--- main.c/formatspecifier.c
+++ main.c/formatspecifier.c
@@ -14,5 +14,13 @@ int main()
     printf("%c\n", currency);
     printf("%s\n", name);
 
-    return 0;
+    int num1 = 1;
+    int num2 = 10;
+    int num3 = 100;
+
+    printf("%d\n", num1);
+    printf("%d\n", num2);
+    printf("%d\n", num3);
+
+        return 0;
 }
