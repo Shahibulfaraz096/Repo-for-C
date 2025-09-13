cf7d7da1fb35f62ab86f6d2e1c22e893d3fb64f4
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-13 12:55:24 +0600

    Maths

---
+++ answers/sdpq.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a = 5, b = 6;
+
+    scanf("%d %d", &a, &b);
+
+    printf(" a + b = %d\n", a + b);
+    printf(" a - b = %d\n", a - b);
+    printf(" a * b = %d\n", a * b);
+    printf(" a / b = %d\n", a / b);
+
+    return 0;
+}
