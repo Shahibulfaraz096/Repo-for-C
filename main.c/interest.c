fd27cd564d2cd12845d98e38c12c7157f5089101
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-14 03:35:26 +0600

    Interest

---
+++ main.c/interest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    float principal, rate, time, interest;
+
+    printf("Enter the principal: ");
+    scanf("%f", &principal);
+
+    printf("Enter the rate: ");
+    scanf("%f", &rate);
+
+    printf("Enter the time: ");
+    scanf("%f", &time);
+
+    interest = (principal * rate * time) / 100;
+
+    printf("The interest is:%.2f\n", interest);
+
+    return 0;
+}
