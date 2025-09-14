e88c435fb7ebee5efee0bbe54fa2a27258c830d7
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-14 22:14:24 +0600

    Leap Year

---
+++ main.c/leapyear.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100))
+    {
+        printf("Leap year.\n");
+    }
+    else
+    {
+        printf("Not a Leap year.\n");
+    }
+
+    return 0;
+}
