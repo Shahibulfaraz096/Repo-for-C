ca39a5d649d50dfca4ca1876f4ff984b8cbd1083
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-14 23:22:44 +0600

    triangle s validity

---
+++ main.c/trianglesvalidity.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter 3 sides of triangles: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        printf("Valid Triangle.\n");
+    }
+    else
+    {
+        printf("Invalid Triangle.\n");
+    }
+
+    return 0;
+}
