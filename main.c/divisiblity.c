2f7bd2ce8429ee31d8ee0441175e063b60f2783d
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-17 03:59:33 +0600

    Check the divisibility by 3 and 5

---
+++ main.c/divisiblity.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 3 == 0 && num % 5 == 0)
+    {
+        printf("Divisible by 3 and 5.\n");
+    }
+    else
+    {
+        printf("Not Divisible by 3 and 5.\n");
+    }
+
+    return 0;
+}
