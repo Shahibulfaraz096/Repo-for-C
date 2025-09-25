7ca424f3c8b49847fdff99ab92df594ccfc1b505
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-09-25 12:07:59 +0600

    Calculator

---
+++ main.c/calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b;
+    char op;
+    printf("Enter expression(e.g., 3 + 4): ");
+    scanf("%f %c %f", &a, &op, &b);
+
+    switch (op)
+    {
+    case '+':
+        printf("Result: %.2f\n", a + b);
+        break;
+    case '-':
+        printf("Result: %.2f\n", a - b);
+        break;
+    case '*':
+        printf("Result: %.2f\n", a * b);
+        break;
+    case '/':
+        if (b != 0)
+        {
+            printf("Result: %.2f\n", a / b);
+        }
+        else
+        {
+            printf("Error: Division by zero.\n");
+        }
+        break;
+    default:
+        printf("Invalid operation.\n");
+    }
+    return 0;
+}
