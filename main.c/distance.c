f30655b42fd8835a94fc97111c0b1a3c054bb400
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2025-11-14 01:48:46 +0600

    Distace

---
+++ main.c/distance.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Point
+{
+    float x, y;
+};
+
+int main()
+{
+    struct Point p1, p2;
+    float distance;
+
+    printf("Enter coordinate of point 1(x y): ");
+    scanf("%f %f", &p1.x, &p1.y);
+    printf("Enter coordinate of point 2(x y): ");
+    scanf("%f %f", &p2.x, &p2.y);
+
+    distance = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+
+    printf("Distance = %.2f\n", distance);
+    return 0;
+}
